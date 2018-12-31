ec81a72f4ce1975edf9772fde4564b5a2d54c007
jackalsin <jackal.sin@hotmail.com>
2018-12-31 14:33:12 -0800

    Add Chapter 1.1.3

---
+++ C#/CSharpInDepth/Chapter1/AutomaticImplementedProperties.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace Chapter1
+{
+    class Product
+    {
+        public string Name { get; private set; }
+        public decimal Price { get; private set; }
+
+        public Product(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        Product()
+        {
+
+        }
+
+        public static List<Product> GetSampleProducts()
+        {
+            return new List<Product>
+            {
+                new Product {Name = "West Side Story", Price = 9.99m },
+                new Product (),
+                new Product("Frogs", 13.99m),
+            };
+
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}: {1}", Name, Price);
+        }
+    }
+}
